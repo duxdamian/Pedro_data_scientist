3a156dec91147e00e6b2fde34b3cf2191f320d30
duxdamian <duxdamian@hotmail.com>
2020-05-01 00:09:10 +0200

    primer archivo

---
+++ lib/00_journalist.rb
@@ -0,0 +1,137 @@
+list_emails = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# 1. Combien y a-t-il de journalistes dans ce array ?
+def nombre_handle(list)
+  return list.size
+end
+
+#  2. Combien d'handle contiennent un numéro ?
+#   #/ Autre methode
+#   if handle =~ /\d/          # prochain Test 
+
+def numero_handle(list)
+  search = 0
+  list.each do |email|
+  email.split("").each do |number|
+      for i in (1..9)
+        if number == String(i)
+          found = true 
+          search += 1 
+          break 
+        end
+      end
+      if found 
+        break 
+      end
+    end
+  end
+return search
+end
+
+
+# 3. Combien d'handle contiennent les 4 lettres du prénom "aude" à la suite ? 
+def aude_handle(list)
+  search = 0
+  list.each do |email|
+      if email.downcase.include? "aude"
+          search += 1
+      end
+  end
+  return search
+end
+
+
+# 4. Combien commencent par une majuscule (première lettre juste après le @) ?
+# Metóde trouve en: https://www.rubyguides.com/2015/06/ruby-regex/
+#                   http://rubytutorial.wikidot.com/metodos-argumentos                      
+
+def commence_mayuscule_handle(list)
+  search = 0
+  list.each do |email|
+      if email[1] =~ /[A-Z]/
+          search += 1
+      end
+  end
+  return search
+end
+
+# Metode que j'utilise normalement
+# def mayuscule_handle(list)
+#  search = 0
+#  search_may = []
+#  list.each do |email|
+#  search_may<<email.downcase
+#  end
+#  list.each_index do |i|
+#  search += 1 if list[i][1] != search_may[i][1]
+#  end
+#  return search
+# end
+
+# 5. Combien contiennent au moins une majuscule ?
+def mayuscule_handle(list)
+  search = 0
+  list.each do |email|                    # https://apidock.com/ruby/String/split
+    email.split("").each do |mayuscule|    # Given that String#split returns an array.Ex. "life is awesome".split => ["life","is","awesome"]
+    for i in 1..9                       # The reverse operation is Array#join.     Ex. "["life","is","awesome"].join(" ")" =>"life is awesome"
+    if mayuscule != i
+    if mayuscule == mayuscule.upcase && mayuscule != "_" && mayuscule != "@"  # Operators toruves dans:
+    found = true                                                              # http://ruby-for-beginners.rubymonstas.org/operators/logical.html
+    search += 1                                                               # https://stackoverflow.com/questions/2249088/ruby-why-does-and-not-work-when-expr-does-not                                                    
+    break 
+    end
+    end
+    end
+    break if found
+  end
+ end
+ return search
+end
+
+# 6. Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
+def underscore_handle(list)
+  search = 0
+  list.each do |email|
+  email.split("").each do |underscore|
+    if underscore == "_"
+      search += 1
+    end
+  end
+  end
+  return search
+end
+
+# 7. Trie la liste de handle par ordre alphabétique.
+def alphabetical_handle(list)
+  list_order = list.sort
+  return list_order
+end
+# alphabetical_handle(list).each_with_index { |e, i| puts "#{i} (#{e})"}
+# https://zestedesavoir.com/tutoriels/634/une-introduction-a-ruby/497_les-bases/2490_les-tableaux/
+# https://parzibyte.me/blog/2019/02/08/ordenar-arreglos-numericos-cadenas-ruby/
+
+# 8. Quels sont les 50 handles les plus courts de ce array ?
+
+
+
+# 9. Quelle est la position dans l'array de la personne @epenser ?
+
+
+
+# - List de commandes del programe ?
+def perform(list)
+  puts "Vóila, dans cette liste il y'a :"
+  puts "1. #{nombre_handle(list)}"
+  puts "2. #{numero_handle(list)}"
+  puts "3. #{aude_handle(list)}"
+  puts "4. #{commence_mayuscule_handle(list)}"
+  puts "5. #{mayuscule_handle(list)}"
+  puts "6. #{underscore_handle(list)}"
+  puts "7. #{alphabetical_handle(list)}"
+  puts "8. "
+  puts "9. "
+
+end
+
+perform(list_emails)
+
